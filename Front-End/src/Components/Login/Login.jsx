903c881fa4dbd26bbbc6c5942c0dfe9405e48fe0
hrushii <hrushiop@gmail.com>
2023-07-11 20:04:28 +0530

    added login and signup

---
+++ Front-End/src/Components/Login/Login.jsx
@@ -0,0 +1,84 @@
+import {React , useState} from 'react'
+import '../../App.css'
+import '../../output.css'
+import { Link } from 'react-router-dom'
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+export default function Login() {
+    const [email,setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [visible, setvisible] = useState(false)
+  return (
+    <div className='min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+                <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
+                    Login to your account
+                </h2>
+        </div>
+        <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md px-4'>
+            <div className='bg-white py-8 px-4 shadow rounded-xl sm:px-10'>
+                <form className='space-y-6'>
+                    <div>
+                        <label htmlFor='email' className='block text-sm font-medium text-gray-700'>Email Address</label>
+                        <div className='mt-1'>
+                            <input type="email" name="email" autoComplete='email' required value={email}  onChange={(e)=>setEmail(e.target.value)} className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400  focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm' />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor='password' className='block text-sm font-medium text-gray-700'>Password</label>
+                        <div className='mt-1 relative'>
+                            <input type={visible?"text":"password"} name="password" autoComplete='current-password' required value={password}  onChange={(e)=>setPassword(e.target.value)} className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400  focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm' />
+                            {
+                                visible ?
+                                (
+                                <AiOutlineEye
+                                    className="absolute right-2 top-2 cursor-pointer"
+                                    size={25} 
+                                    onClick={()=>setvisible(false)}
+                                    />
+                                )
+                                    :
+                                    (
+                                        <AiOutlineEyeInvisible
+                                        className="absolute right-2 top-2 cursor-pointer"
+                                        size={25}
+                                        onClick={()=>setvisible(true)}
+                                    /> 
+                                    ) 
+                            }
+                        </div>
+                    </div>
+                    <div>
+                        <div className='flex items-center justify-between'>
+                            <div className='flex items-center'>
+                            <input type="checkbox" name="remember-me" id="remember-me" className='h-4 w-4 text-blue-400 focus:ring-blue-500 border-gray-300 rounded' />
+                                <label htmlFor="remember-me" className='ml-2 block text-sm text-gray-900'>Remember Me</label>
+                            </div>
+                            
+                            
+                                
+                               
+                                    <a href='.forgot-password' className='text-sm ml-auto font-medium text-blue-500 hover:text-blue-700'>
+                                        Forgot Password?
+                                    </a>
+                                
+                            
+                        </div>
+                    </div>
+                    <div>
+              <button class="appearance-none w-full h-[40px]  py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700">Submit</button>
+                       
+                    </div>
+                    <div className='flex items-center w-full'>
+                        <h4>Dont' have a account?</h4>
+                            <Link to='/sign_up' className='text-blue-600 pl-2'>
+                                Sign up
+                            </Link>
+                    </div>
+                    
+                </form>
+
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ Front-End/src/Routes.js
@@ -0,0 +1,6 @@
+import { LoginPage } from "./pages/Login";
+import { SignUpPage  } from "./pages/SignUpPage";
+export{
+    LoginPage,
+    SignUpPage,
+};
