1844e97adf2c00ef768dd3bfe5154e64d9d06c94
hrushii <hrushiop@gmail.com>
2023-07-14 22:03:27 +0530

    d

--- Front-End/src/server.js
+++ Front-End/src/server.js
@@ -1,2 +1,2 @@
 //export const server = "http://localhost:8000/api/v2";
-export const server = "https://e-com-rushiii3.vercel.app:8000/api/v2";
+export const server = "https://e-com-rushiii3.vercel.app/api/v2";
